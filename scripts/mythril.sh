7a6e68b6d9f4213fee19113550dab69a374cde90
fahust <kevin.dellova@hotmail.fr>
2022-11-05 18:48:05 +0100

    scripts

---
+++ scripts/mythril.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SOLC_VERSION="${SOLC_VERSION:-0.8.6}"
+echo "Using solc version $SOLC_VERSION"
+
+shopt -s globstar
+for i in contracts/**/*.sol; do # Whitespace-safe and recursive
+    echo "Starting mythril analysis of contract $i"
+    # Run mythril container
+    docker run --rm -it -v $(pwd):/tmp mythril/myth analyze --solc-json /tmp/mythril.json --solv $SOLC_VERSION /tmp/$i
+done
